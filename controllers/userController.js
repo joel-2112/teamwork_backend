b5af5cd9b7abda35d1a9b900711f329b1a0c68e6
joel-2112 <eyueljoel21@gmail.com>
2025-05-08 15:57:13 +0300

    the github action and the workflow works successfully

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,6 +13,28 @@ const User = require('../models/User')
     });
   }
 } 
+//get user by id
+const getUserById = async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: 'User not found',
+      });
+    }
+    res.status(200).json({
+      success: true,
+      data: user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Server Error',
+    });
+  }
+};
 module.exports = {
   getAllUsers,
+  getUserById
 };
