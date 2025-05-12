bd41b77cfa708366455dfd3eb17bd005439fce5a
joel-2112 <eyueljoel21@gmail.com>
2025-05-12 11:46:56 +0300

    the database host configured well

--- config/database.js
+++ config/database.js
@@ -5,9 +5,9 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST + '.oregon-postgres.render.com',
+    host: process.env.DB_HOST + process.env.DB_APPEND,
     port: process.env.DB_PORT,
-    dialect: 'postgres',
+    dialect: process.env.DB_DIALECT,
     dialectOptions: {
       ssl: {
         require: true,
