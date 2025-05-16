0715d4bf8b29452d49fc6d15668ce624650dd137
joel-2112 <eyueljoel21@gmail.com>
2025-05-16 10:54:08 +0300

    Fix region-hierarchy route conflict

--- routes/agentRoutes.js
+++ routes/agentRoutes.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 const AgentController = require('../controllers/agentController');
 
+router.get('/region-hierarchy', AgentController.getRegionHierarchy);
+router.get('/regions/:regionId/zones', AgentController.getZonesByRegion);
+router.get('/zones/:zoneId/woredas', AgentController.getWoredasByZone);
 router.post('/', AgentController.createAgent);
 router.get('/', AgentController.getAllAgents);
 router.get('/:id', AgentController.getAgentById);
 router.put('/:id', AgentController.updateAgent);
 router.delete('/:id', AgentController.deleteAgent);
-router.get('/region-hierarchy', AgentController.getRegionHierarchy);
-router.get('/regions/:regionId/zones', AgentController.getZonesByRegion);
-router.get('/zones/:zoneId/woredas', AgentController.getWoredasByZone);
+
 
 module.exports = router;
