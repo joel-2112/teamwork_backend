57baebd235363a9cf38a3d3fafafb3506d42c004
joel-2112 <eyueljoel21@gmail.com>
2025-05-18 22:07:37 +0300

    feat:the ci/cd integrated

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(express.urlencoded({ extended: true }));
 // Routes
 app.get('/', (req, res) => {
     res.json({
-        message: 'Welcome to teamwork software company API',
+        message: 'Welcome to teamwork',
         version: '1.0',
         endpoints: '/api/v1/{auth,users,jobs,job-applications}',
     });
