1b2592ffd9551b5de9b442b3bbe99c9181c15c71
joel-2112 <eyueljoel21@gmail.com>
2025-05-18 22:36:17 +0300

    auto deployment

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(express.urlencoded({ extended: true }));
 // Routes
 app.get('/', (req, res) => {
     res.json({
-        message: 'Welcome to teamwork system development',
+        message: 'Welcome to teamwork software!!',
         version: '1.0',
         endpoints: '/api/v1/{...version1}',
     });
