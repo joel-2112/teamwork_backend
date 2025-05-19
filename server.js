cb982c03bdcb72f65f0a8054def3154520acf7b7
joel-2112 <eyueljoel21@gmail.com>
2025-05-19 10:16:39 +0300

    the main api gateway itegrate

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(express.urlencoded({ extended: true }));
 // Routes
 app.get('/', (req, res) => {
     res.json({
-        message: 'Welcome to teamwork',
+        message: 'Welcome to teamwork the main api gateway',
         version: '1.0',
         endpoints: '/api/v1/{...version1}',
     });
