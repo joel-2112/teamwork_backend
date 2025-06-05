b7d99b12678259e16f06c4127f68bc57d1e410c4
joel-2112 <eyueljoel21@gmail.com>
2025-06-05 12:00:14 +0300

    the routes work properly

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(express.urlencoded({ extended: true }));
 // Routes
 app.get('/', (req, res) => {
     res.json({
-        message: 'Welcome to teamwork IT solution PLC',
+        message: 'Welcome to teamwork IT solution ',
         version: '1.0',
         endpoints: '/api/v1 => the first version ',
     });
