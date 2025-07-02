0bd22f9feb64988f556d1439a7f5042f5e8bae77
Zwubman <zwubeman@gmail.com>
2025-07-02 05:25:12 -0400

    complet the function related to role.

--- routes/newsRoutes.js
+++ routes/newsRoutes.js
@@ -1,24 +1,48 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
 import {
-    createNewsController,
-    getAllNewsController,
-    getNewsByIdController,
-    updateNewsController,
-    deleteNewsController
-} from '../controllers/newsController.js';
-import { protect, requireRole } from '../middlewares/authMiddleware.js';
-import { validateRequest } from '../middlewares/validators/validateRequest.js';
-import { createNewsValidator, validateParamId } from "../middlewares/validators/authValidator.js"
-import upload from '../middlewares/upload.js'; 
-
+  createNewsController,
+  getAllNewsController,
+  getNewsByIdController,
+  updateNewsController,
+  deleteNewsController,
+} from "../controllers/newsController.js";
+import { protect, requireRole } from "../middlewares/authMiddleware.js";
+import { validateRequest } from "../middlewares/validators/validateRequest.js";
+import {
+  createNewsValidator,
+  validateParamId,
+} from "../middlewares/validators/authValidator.js";
+import upload from "../middlewares/upload.js";
 
 //the news routes
-router.post('/create-news', upload.single('picture'), createNewsValidator, validateRequest, protect, requireRole('admin'), createNewsController);
-router.put('/update-news/:id', protect, requireRole('admin'), updateNewsController);
-router.delete('/delete-news/:id', validateParamId, validateRequest, deleteNewsController);
-router.get('/news/:id', validateParamId, validateRequest, getNewsByIdController);
-router.get('/all-news', getAllNewsController);
-
+router.post(
+  "/create-news",
+  upload.single("picture"),
+  createNewsValidator,
+  validateRequest,
+  protect,
+  requireRole("admin"),
+  createNewsController
+);
+router.put(
+  "/update-news/:id",
+  protect,
+  requireRole("admin"),
+  updateNewsController
+);
+router.delete(
+  "/delete-news/:id",
+  validateParamId,
+  validateRequest,
+  deleteNewsController
+);
+router.get(
+  "/news/:id",
+  validateParamId,
+  validateRequest,
+  getNewsByIdController
+);
+router.get("/all-news", getAllNewsController);
 
 export default router;
--- routes/roleRoute.js
+++ routes/roleRoute.js
@@ -1,11 +1,50 @@
 import express from "express";
-import { createRoleController } from "../controllers/roleController.js";
-import { createRoleValidator } from "../middlewares/validators/authValidator.js";
+import {
+  createRoleValidator,
+  validateParamId,
+} from "../middlewares/validators/authValidator.js";
+import {
+  createRoleController,
+  updateRoleById,
+  getRoleById,
+  deleteRoleById,
+} from "../controllers/roleController.js";
+import { protect, requireRole } from "../middlewares/authMiddleware.js";
 import { validateRequest } from "../middlewares/validators/validateRequest.js";
 
 const router = express.Router();
 
-router.post('/create-role', createRoleValidator, validateRequest, createRoleController);
-
+router.post(
+  "/create-role",
+  protect,
+  requireRole("admin"),
+  createRoleValidator,
+  validateRequest,
+  createRoleController
+);
+router.put(
+  "/update-role/:id",
+  protect,
+  requireRole("admin"),
+  validateParamId,
+  validateRequest,
+  updateRoleById
+);
+router.get(
+  "/role/:id",
+  protect,
+  requireRole("admin"),
+  validateParamId,
+  validateRequest,
+  getRoleById
+);
+router.delete(
+  "/delete-role/:id",
+  protect,
+  requireRole("admin"),
+  validateParamId,
+  validateRequest,
+  deleteRoleById
+);
 
 export default router;
