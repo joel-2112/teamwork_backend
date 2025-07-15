ddd38c26bb6016360d20ffde4187e6893d98198c
Zwubman <zwubeman@gmail.com>
2025-07-15 05:59:05 -0400

    fix update agent data issue related to admin

--- routes/agentRoutes.js
+++ routes/agentRoutes.js
@@ -16,7 +16,7 @@ import { protect, requireRole } from "../middlewares/authMiddleware.js";
 router.post("/apply-agent", protect, createAgent);
 router.get("/all-agents", protect, requireRole("admin"), getAllAgents);
 router.get("/agent/:id", protect, requireRole("admin"), getAgentById);
-router.put("/update/:id", protect, requireRole("admin"), updateAgent);
+router.put("/update/:id", protect, updateAgent);
 router.delete("/delete/:id", protect, requireRole("admin"), deleteAgent);
 router.put("/update-status/:id", protect, requireRole("admin"), updateAgentStatus);
 router.get("/deleted-agents", protect, requireRole("admin"), getAllDeletedAgent);
