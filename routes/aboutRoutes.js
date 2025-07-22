89f6edf58329951fce51e5c6706c99c9c3ebef61
Zwubman <zwubeman@gmail.com>
2025-07-22 09:35:33 -0400

    protect about route

--- routes/aboutRoutes.js
+++ routes/aboutRoutes.js
@@ -9,11 +9,12 @@ import {
   deleteAbout,
 } from "../controllers/aboutController.js";
 import upload from "../middlewares/upload.js";
+import { protect, requireRole } from "../middlewares/authMiddleware.js";
 
-router.post("/create-about", upload.single("aboutImage"), createAbout);
+router.post("/create-about", protect, requireRole('admin'), upload.single("aboutImage"), createAbout);
 router.get("/all-abouts", getAllAbout);
 router.get("/about/:id", getAboutById);
-router.put("/update/:id", upload.single("aboutImage"), updateAbout);
-router.delete("/delete/:id", deleteAbout);
+router.put("/update/:id", protect, requireRole('admin'), upload.single("aboutImage"), updateAbout);
+router.delete("/delete/:id", protect, requireRole('admin'), deleteAbout);
 
 export default router;
