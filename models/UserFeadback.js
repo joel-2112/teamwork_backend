04c66961c66911bcc5ea6a0a993c0a61cac471bf
Zwubman <zwubeman@gmail.com>
2025-07-25 03:25:04 -0400

    fix enum value of gender in user feedback

--- models/UserFeadback.js
+++ models/UserFeadback.js
@@ -37,7 +37,7 @@ export default (db, DataTypes) => {
         allowNull: true,
       },
       gender: {
-        type: DataTypes.ENUM("Male", "Female", "Other"),
+        type: DataTypes.ENUM("male", "female", "other"),
         allowNull: false,
       },
       phone: {
