101d2e21265ad8bfa1b639b85340e957cb1b7e8a
Zwubman <zwubeman@gmail.com>
2025-08-05 13:20:40 -0400

    permit all admin, region admin, zone admin and woreda admin to veiw service order based on thiere location

--- routes/serviceOrderRoutes.js
+++ routes/serviceOrderRoutes.js
@@ -9,20 +9,39 @@ import {
   updateServiceOrderStatus,
   cancelOrder,
   getMyOrders,
-  orderStatistics
+  orderStatistics,
 } from "../controllers/serviceOrderController.js";
 import { protect, requireRole } from "../middlewares/authMiddleware.js";
-import upload from "../middlewares/upload.js"
+import upload from "../middlewares/upload.js";
 
-
-router.post("/create-order", protect, upload.single("requirementFile"), createServiceOrder);
-router.get("/all-orders", protect, requireRole('admin'), getAllOrders);
-router.get("/order/:id", protect, requireRole('admin'), getOrderById);
+router.post(
+  "/create-order",
+  protect,
+  upload.single("requirementFile"),
+  createServiceOrder
+);
+router.get(
+  "/all-orders",
+  protect,
+  requireRole("admin", "regionAdmin", "zoneAdmin", "woredaAdmin"),
+  getAllOrders
+);
+router.get(
+  "/order/:id",
+  protect,
+  requireRole("admin", "regionAdmin", "zoneAdmin", "woredaAdmin"),
+  getOrderById
+);
 router.put("/update/:id", protect, updateOrder);
 router.delete("/delete/:id", protect, deleteOrder);
-router.put("/update-status/:id", protect, requireRole('admin'), updateServiceOrderStatus);
+router.put(
+  "/update-status/:id",
+  protect,
+  requireRole("admin"),
+  updateServiceOrderStatus
+);
 router.put("/cancel-order/:id", protect, cancelOrder);
 router.get("/my-orders", protect, getMyOrders);
-router.get("/order-stat", protect, requireRole('admin'), orderStatistics)
+router.get("/order-stat", protect, requireRole("admin"), orderStatistics);
 
 export default router;
