d556807233534fb3d4ed1d3e5fb7767fea5bcf01
Zwubman <zwubeman@gmail.com>
2025-08-13 04:15:52 -0400

    fix some validator

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,16 +9,12 @@ import {
   logout,
   checkAuth,
 } from "../controllers/authController.js";
-import {
-  createUserValidator,
-  loginValidator,
-} from "../middlewares/validators/authValidator.js";
 import { validateRequest } from "../middlewares/validators/validateRequest.js";
 import { protect, requireRole } from "../middlewares/authMiddleware.js";
 
 
-router.post("/send-otp", createUserValidator, validateRequest, sendOtp);
-router.post("/login", loginValidator, validateRequest, login);
+router.post("/send-otp",  sendOtp);
+router.post("/login",  login);
 router.post("/verify-otp", verifyOtp);
 router.post("/refresh-token", refreshToken);
 router.post("/logout", logout);
